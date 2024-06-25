9029392bd911a53e43097199baa953094d2610e7
Martín Cristaldo <cristaldopmartin@gmail.com>
2024-06-24 23:29:41 -0300

    first commit

---
+++ app-example/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs>aaa
+    </Tabs>
+  );
+}
